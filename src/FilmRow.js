0ac301a351ba83e6c008af61356d35ff7a1d5fca
emsleyd <emsleyd@gmail.com>
2022-10-12 16:37:55 +0100

    filmrow.js created

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import TMDB from "./TMDB";
 
 export default class FilmRow extends Component {
   render() {
@@ -7,8 +8,8 @@ export default class FilmRow extends Component {
         <img src={posterUrl} alt="" />
       
         <div className="film-summary">
-          <h1>TITLE</h1>
-          <p>YEAR</p>
+          <h1>{TMDB.films.id.title}</h1>
+          <p>{TMDB.films.id.release_date}</p>
         </div>
       </div>
     )
