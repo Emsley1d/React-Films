1e9da76a4223429a3c3cbe6d7343476c4aa98b4e
emsleyd <emsleyd@gmail.com>
2022-10-16 10:12:40 +0100

    step 9?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import React, { Component } from 'react'
-import Details from './Details'
-import Films from './Films'
-import TMDB from './TMDB'
+import React, { Component } from 'react';
+import './App.css';
+import Details from './Details.js';
+import Films from './Films.js';
+import TMDB from './TMDB.js';
 
-export default class App extends Component {
+const films = TMDB.films
+
+
+class App extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        films: films,
+        faves: [],
+        current: {}
+      }
+
+      this.handleFaveToggle = this.handleFaveToggle.bind(this)
+    }
+
+    handleFaveToggle(film) {
+      const faves = this.state.faves.slice()
+      const filmIndex = this.state.filmindex.indexOf()
+
+
+
+    // if film in array will return indexOf() starting at 0
+    // if film not found in array then indexOf() return 01
+
+      (filmIndex === -1) ?
+      faves.push(film):
+      faves.splice(filmIndex, 1) 
+    }
+
+
+//     If the film is already in their favorites, then when the user clicks the button, they want to remove it from their favorites. You need to take it out of the faves array.
+// If the film is not in their favorites, then when the user clicks the button, they want to add it to their favorites. You need to add it to the faves array.
+
+    // function onFaveToggle() {
+    //   if (film == 'fave') { 
+    //   console.log(`Adding {this.props.film.title} to faves`)
+    //   }
+    //   else 
+    //   console.log(`Removing {this.props.film.title} to faves`)
+    // };
 
   render() {
 
     return (
-      <div>
-        <Films films={TMDB.films} ></Films>
-        <Details films={TMDB.films} ></Details>
+      <div className="App" >
+        <div className="film-library">
+          <Films films={this.props.films} />
+          <Details films={this.props.current} />
+        </div>
       </div>
-
-
-    )
-    }
+    );
   }
+}
 
-
-
-
+export default App;
--- src/Fave.js
+++ src/Fave.js
@@ -3,26 +3,31 @@ import React, { Component } from 'react'
 
 export default class Fave extends Component {
 
-    state = {
-        isFave: false
-    }
+  // constructor(props) {
+  //   super(props);
 
+  //   this.state = {
+  //     isFave: false,
+  //   }
+
+  // }
 
     handleClick=(e) => {
           e.stopPropagation()
-          this.setState({
-          isFave: !this.state.isFave
-      })
+          console.log('Handling Fave click!')
+          this.props.onFaveToggle()
     }
+    
 
     render() {
 
-    const isFave = (this.state.isFave) ? 'remove_from_queue' : 'add_to_queue'
+    const isFave = (this.props.onFaveToggle) ? 'remove_from_queue' : 'add_to_queue'
 
     return (
-        <div className="film-row-fave add_to_queue" onClick={this.handleClick}> 
-        <p className="material-icons">add_to_queue</p>
+      <div className={`film-row-fave ${onFaveToggle}`} onClick={(e) => this.handleClick(e)}>
+        <p className="material-icons">{onFaveToggle}</p>
       </div>
+
     )
   }
 }
